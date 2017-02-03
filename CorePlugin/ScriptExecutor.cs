1d7f7c8dd9fc5b837a4d8da4f5174abed38a7ea3
Alexandre Oliveira <rockytvbr@gmail.com>
2017-02-03 15:04:15 -0200

    Use our custom ExecutionEngine

--- CorePlugin/ScriptExecutor.cs
+++ CorePlugin/ScriptExecutor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Duality;
-using PythonScripting.Resources;
+using RockyTV.Duality.Plugins.IronPython.Resources;
 
 using IronPython.Hosting;
 using IronPython.Runtime;
@@ -12,18 +12,28 @@ using IronPython.Compiler;
 using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
 
-namespace PythonScripting
+namespace RockyTV.Duality.Plugins.IronPython
 {
 	public class ScriptExecutor : Component, ICmpInitializable, ICmpUpdatable
 	{
         public ContentRef<PythonScript> Script { get; set; }
 
+        [DontSerialize]
+        private PythonExecutionEngine _engine;
+        protected PythonExecutionEngine Engine
+        {
+            get { return _engine; }
+        }
+
 		public void OnInit(InitContext context)
         {
-            if (context == InitContext.Activate)
-            {
+            if (!Script.IsAvailable) return;
 
-            }
+            _engine = new PythonExecutionEngine(Script.Res.Content);
+            _engine.SetVariable("gameObject", GameObj);
+
+            if (context == InitContext.Activate)
+                _engine.CallMethod("start");
         }
 
         public void OnUpdate()
