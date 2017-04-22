343f59ea82a22ab2d88e83f6b84cb4eec721934c
Alexandre Oliveira <rockytvbr@gmail.com>
2017-04-22 15:35:48 -0300

    Add ICmpRenderer support

--- CorePlugin/ScriptExecutor.cs
+++ CorePlugin/ScriptExecutor.cs
@@ -12,12 +12,13 @@ using IronPython.Compiler;
 using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
 using Duality.Editor;
+using Duality.Drawing;
 
 namespace RockyTV.Duality.Plugins.IronPython
 {
     [EditorHintCategory(Properties.ResNames.CategoryScripts)]
     [EditorHintImage(Properties.ResNames.IconScriptGo)]
-    public class PythonScriptExecutor : Component, ICmpInitializable, ICmpUpdatable
+    public class PythonScriptExecutor : Component, ICmpInitializable, ICmpUpdatable, ICmpRenderer
     {
         public ContentRef<PythonScript> Script { get; set; }
 
@@ -33,7 +34,18 @@ namespace RockyTV.Duality.Plugins.IronPython
             get { return Time.MsPFMult * Time.TimeMult; }
         }
 
-        public void OnInit(InitContext context)
+		public float BoundRadius
+		{
+			get
+			{
+				if (_engine != null)
+					if (_engine.HasMethod("bound_radius"))
+						return Convert.ToSingle(_engine.GetVariable("bound_radius"));
+				return 0.0f;
+			}
+		}
+
+		public void OnInit(InitContext context)
         {
             if (Script.IsExplicitNull || !Script.IsAvailable) return;
 
@@ -60,5 +72,26 @@ namespace RockyTV.Duality.Plugins.IronPython
             if (context == ShutdownContext.Deactivate)
                 GameObj.DisposeLater();
         }
-    }
+
+		public bool IsVisible(IDrawDevice device)
+		{
+			if (_engine != null)
+			{
+				if (_engine.HasMethod("isVisible"))
+				{
+					bool isVisible;
+					if (bool.TryParse(Convert.ToString(_engine.CallFunction("isVisible", device)), out isVisible))
+						return isVisible;
+				}
+			}
+			return false;
+		}
+
+		public void Draw(IDrawDevice device)
+		{
+			if (_engine != null)
+				if (_engine.HasMethod("draw"))
+					_engine.CallMethod("draw", device);
+		}
+	}
 }
